88bdc59d42873bbcf84df7197c0998b74d588a1c
HELLBOY017 <abhaygill017@gmail.com>
2022-02-19 09:18:37 +0000

    sm8250-common: Switch to DaxUI for dolby - Remove dolby stagefright blobs as dolby codec decoding isn't supported by aosp
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- DeviceSettings/src/org/aosp/device/DeviceSettings/DeviceSettings.java
+++ DeviceSettings/src/org/aosp/device/DeviceSettings/DeviceSettings.java
@@ -63,7 +63,6 @@ public class DeviceSettings extends PreferenceFragment
     private static final String FILE_LEVEL = "/sys/devices/platform/soc/a8c000.i2c/i2c-3/3-005a/leds/vibrator/level";
     public static final String DEFAULT = "3";
 
-    private DolbySwitch mDolbySwitch;
     private Preference mDozeSettings;
 
     private static SwitchPreference mFpsInfo;
@@ -76,7 +75,6 @@ public class DeviceSettings extends PreferenceFragment
     private static TwoStatePreference mEdgeTouchSwitch;
     private static TwoStatePreference mAutoHBMSwitch;
     private static TwoStatePreference mMuteMedia;
-    private static TwoStatePreference mEnableDolbyAtmos;
 
     private VibratorStrengthPreference mVibratorStrengthPreference;
     private CustomSeekBarPreference mFpsInfoTextSizePreference;
@@ -103,11 +101,6 @@ public class DeviceSettings extends PreferenceFragment
         mHBMModeSwitch.setEnabled(HBMModeSwitch.isSupported());
         mHBMModeSwitch.setChecked(PreferenceManager.getDefaultSharedPreferences(getContext()).getBoolean(DeviceSettings.KEY_HBM_SWITCH, false));
         mHBMModeSwitch.setOnPreferenceChangeListener(this);
-
-        mDolbySwitch = new DolbySwitch(this.getContext());
-        mEnableDolbyAtmos = (TwoStatePreference) findPreference(KEY_ENABLE_DOLBY_ATMOS);
-        mEnableDolbyAtmos.setChecked(mDolbySwitch.isCurrentlyEnabled());
-        mEnableDolbyAtmos.setOnPreferenceChangeListener(this);
 
         mDozeSettings = (Preference)findPreference(PREF_DOZE);
         mDozeSettings.setOnPreferenceClickListener(preference -> {
@@ -190,8 +183,6 @@ public class DeviceSettings extends PreferenceFragment
             } else {
                 this.getContext().stopService(hbmIntent);
             }
-        } else if (preference == mEnableDolbyAtmos) {
-            mDolbySwitch.setEnabled((Boolean) newValue);
         } else if (preference == mFpsInfo) {
             boolean enabled = (Boolean) newValue;
             Intent fpsinfo = new Intent(this.getContext(), FPSInfoService.class);
